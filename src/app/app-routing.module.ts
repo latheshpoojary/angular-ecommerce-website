462b43b70fec53d5f975a4eb111f92b0b87b020c
lathesh <latheshpoojary2639@gmail.com>
2023-07-12 07:41:16 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './component/products/products.component';
+import { CartsComponent } from './component/carts/carts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'product',pathMatch:'full'},
+  {path:'product',component:ProductsComponent},
+  {path:'cart',component:CartsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
